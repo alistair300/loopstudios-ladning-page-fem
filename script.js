b36fa5caf7eb6ad93f5a2d29e93820c9fdec7e86
Alistair <alistairfernandes91371@gmail.com>
2021-06-11 11:24:05 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const overlay = document.querySelector('.hero__overlay');
+const links = document.querySelector('.hero__links');
+const navToggler = document.querySelector('.hero__toggle');
+
+navToggler.addEventListener('click', () => {
+    overlay.classList.toggle('open');
+    links.classList.toggle('open');
+    navToggler.classList.toggle('open');
+});
